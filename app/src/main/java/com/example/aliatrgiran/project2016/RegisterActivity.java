bd5ae20154ad308652ae702c9e06162eb3f83524
Ali Atrgiran <aliatrgiran@Alis-MacBook-Pro.local>
2016-04-18 16:08:32 +0900

    second commit

--- app/src/main/java/com/example/aliatrgiran/project2016/RegisterActivity.java
+++ app/src/main/java/com/example/aliatrgiran/project2016/RegisterActivity.java
@@ -1,17 +1,13 @@
 package com.example.aliatrgiran.project2016;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_register);
     }
 }
